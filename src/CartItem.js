f844d74ac064cb7bb91c626f8a0e071b3cdc7edf
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-03 13:49:17 +0530

    adding constructor and using super keyword and destructuring the object

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,18 +1,31 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+      constructor() {
+            super();
+            this.state = {
+                  price: 999,
+                  title: "Mobile Phone",
+                  qty: 1,
+                  img: ""
+            }
+      }
       render() {
+            const { price, title, qty, } = this.state;
             return (
                   <div className='cart-item'>
                         <div className='left-block'>
                               <img style={styles.image} />
                         </div>
                         <div className='right-block'>
-                              <div style={{ fontSize: 25 }}>Phone</div>
-                              <div style={{ color: '#777' }}>Rs 999</div>
-                              <div style={{ color: '#777' }}>Qty 1</div>
+                              <div style={{ fontSize: 25 }}>{title}</div>
+                              <div style={{ color: '#777' }}>Rs {price}</div>
+                              <div style={{ color: '#777' }}>Qty {qty}</div>
                               <div className='cart-item-action'>
                                     {/* Buttons */}
+                                    <img alt="increase" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1828/1828919.png' />
+                                    <img alt="decrease" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1828/1828899.png' />
+                                    <img alt="delete" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/9068/9068885.png' />
                               </div>
                         </div>
                   </div>
@@ -22,7 +35,7 @@ class CartItem extends React.Component {
 
 const styles = {
       image: {
-            height: 100,
+            height: 120,
             width: 110,
             borderRadius: 4,
             background: '#ccc',

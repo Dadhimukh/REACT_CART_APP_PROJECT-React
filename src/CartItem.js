86bfecb5a3a364a6970980f441c7ff1b42233b3c
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-06 13:59:22 +0530

    Adding EventListener to the Increase Button and setState the qty increasing by one

--- src/CartItem.js
+++ src/CartItem.js
@@ -9,7 +9,26 @@ class CartItem extends React.Component {
                   qty: 1,
                   img: ""
             }
+            // this.increaseQuantity = this.increaseQuantity.bind(this);
       }
+
+      // increasingQuantity Function
+      increaseQuantity = () => {
+            // this.state.qty += 1;
+            // console.log('this' , this.state);
+            // setting state==================================> Way 1======> if previous State is not required then use this
+            // this.setState({
+            //      qty : this.state.qty + 1;
+            // });
+
+            // setting state==================================> Way 2======> if previous State is required then use this
+            this.setState((prevState) => {
+                  return {
+                        qty: prevState.qty + 1,
+                  }
+            });
+      }
+
       render() {
             const { price, title, qty, } = this.state;
             return (
@@ -23,9 +42,24 @@ class CartItem extends React.Component {
                               <div style={{ color: '#777' }}>Qty {qty}</div>
                               <div className='cart-item-action'>
                                     {/* Buttons */}
-                                    <img alt="increase" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1828/1828919.png' />
-                                    <img alt="decrease" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/1828/1828899.png' />
-                                    <img alt="delete" className='action-icons' src='https://cdn-icons-png.flaticon.com/128/9068/9068885.png' />
+                                    <img
+                                          alt="increase"
+                                          className='action-icons'
+                                          src='https://cdn-icons-png.flaticon.com/128/1828/1828919.png'
+                                          onClick={this.increaseQuantity}
+                                    />
+
+                                    <img
+                                          alt="decrease"
+                                          className='action-icons'
+                                          src='https://cdn-icons-png.flaticon.com/128/1828/1828899.png'
+                                    />
+
+                                    <img
+                                          alt="delete"
+                                          className='action-icons'
+                                          src='https://cdn-icons-png.flaticon.com/128/9068/9068885.png'
+                                    />
                               </div>
                         </div>
                   </div>

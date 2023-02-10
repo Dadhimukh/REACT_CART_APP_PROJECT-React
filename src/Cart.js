e107ebc112896c7a73c708220de5a53ff1862c35
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-10 12:37:51 +0530

    Showing the total price of item present in cart

--- src/Cart.js
+++ src/Cart.js
@@ -2,26 +2,26 @@ import React from 'react';
 import CartItem from './CartItem';
 
 const Cart = (props) => {
-    const { products } = props;
-    return (
-      <div className='cart'>
-        {products.map((product) => {
-          return (
-            <CartItem
-              product={product}
-              key={product.id}
-              onIncreaseQuantity={props.onIncreaseQuantity}
-              onDecreaseQuantity={props.onDecreaseQuantity}
-              onDeleteProduct={props.onDeleteProduct}
-            />
-          )
-        })}
+  const { products } = props;
+  return (
+    <div className='cart'>
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.id}
+            onIncreaseQuantity={props.onIncreaseQuantity}
+            onDecreaseQuantity={props.onDecreaseQuantity}
+            onDeleteProduct={props.onDeleteProduct}
+          />
+        )
+      })}
 
-      </div>
+    </div>
 
-    );
+  );
 
-  };
+};
 
 
 

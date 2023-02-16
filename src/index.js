c18d87f68895f41f2d2cbe5044199a5d48264842
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-16 14:03:23 +0530

    installing fire base and importing in index.js file

--- src/index.js
+++ src/index.js
@@ -2,11 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBOeMAN1ZeCKNH12yVVdWg-hqi_Hi_MnJo",
+  authDomain: "cart-2f71d.firebaseapp.com",
+  projectId: "cart-2f71d",
+  storageBucket: "cart-2f71d.appspot.com",
+  messagingSenderId: "679078526300",
+  appId: "1:679078526300:web:f0e14fdacd0a110e4e1599"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<App />
+  <App />
 );
 
 

1fedde42f3192bfa3b22fec30b30870c129de269
Dadhimukh Yadav <yadavdadhimukh@gmail.com>
2023-02-17 18:48:41 +0530

    Connected to firebase and fatching data from firebase

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
-  import React from "react";
+import React from "react";
+import { firestore } from "./firebase";
+
 // import './App.css';
 import Cart from "./Cart";
-import Navbar from './NavBar';
+import Navbar from "./NavBar";
 
 class App extends React.Component {
-
   constructor() {
     super();
     this.state = {
-      products: []
-    }
+      products: [],
+    };
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
+
+  componentDidMount() {
+    //fetching all the products from the cloud firestore
+    firestore
+      //query for fecthing the product which we want as per our query
+      .collection("porducts") //getting all the products
+      // .where('price','>=', 999) // after fetching db we should write query
+      .onSnapshot((snapshot) => {
+        const products = snapshot.docs.map((doc) => {
+          const data = doc.data();
+          data["id"] = doc.id;
+          return data;
+        });
+
+        this.setState({
+          products: products,
+          loading: false,
+        });
+      });
+  }
+
   // Increasing Quantity
   handleIncreaseQuantity = (product) => {
     const { products } = this.state;
@@ -19,9 +41,9 @@ class App extends React.Component {
     products[index].qty += 1;
     this.setState({
       // products:products
-      products
-    })
-  }
+      products,
+    });
+  };
 
   // Decreasing Quantity
   handleDecreaseQuantity = (product) => {
@@ -35,19 +57,18 @@ class App extends React.Component {
 
     this.setState({
       // products:products
-      products
-    })
-  }
+      products,
+    });
+  };
 
   // Deleating Products
   handleDeleteProduct = (id) => {
     const { products } = this.state;
     const items = products.filter((item) => item.id !== id);
     this.setState({
-      products: items
-    })
-  }
-
+      products: items,
+    });
+  };
 
   getCartCount = () => {
     const { products } = this.state;
@@ -58,16 +79,16 @@ class App extends React.Component {
     });
 
     return count;
-  }
+  };
 
-  getCartTotal = () =>{
+  getCartTotal = () => {
     const { products } = this.state;
     let cartTotal = 0;
     products.map((product) => {
-      cartTotal = cartTotal + product.qty * product.price
+      cartTotal = cartTotal + product.qty * product.price;
     });
     return cartTotal;
-  }
+  };
 
   render() {
     const { products } = this.state;
@@ -80,7 +101,9 @@ class App extends React.Component {
           onDecreaseQuantity={this.handleDecreaseQuantity}
           onDeleteProduct={this.handleDeleteProduct}
         />
-        <div style={{fontSize:20,padding:10,fontWeight:"bold"}}>TOTAL:{this.getCartTotal()}</div>
+        <div style={{ fontSize: 20, padding: 10, fontWeight: "bold" }}>
+          TOTAL:{this.getCartTotal()}
+        </div>
       </div>
     );
   }
